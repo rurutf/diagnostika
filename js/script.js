c7beb4d1f8c520e4152a090064ccc6981e65f283
Inna Pavlyutenkova <i.pavlyutenkova@devspark.ru>
2021-03-25 15:51:14 +0300

    add tabs and accordions

--- js/script.js
+++ js/script.js
@@ -2,3 +2,21 @@ document.getElementById('trigger').addEventListener('click', () => {
     document.querySelector('.nav__list').classList.toggle('show');
     document.querySelector('.mobile-menu__btn').classList.toggle('open');
 });
+
+let acc = document.getElementsByClassName("accordeon__header");
+let i;
+
+
+for (i=0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+
+        let main = this.nextElementSibling;
+        if (main.style.display === "block") {
+            main.style.display = "none";
+        } else {
+            main.style.display = "block"
+        }
+
+    });
+}
