2bebaec0db76eddf25567584de9a2d9a985f80f0
Inna Pavlyutenkova <i.pavlyutenkova@devspark.ru>
2021-03-25 08:49:16 +0300

    add all files

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('trigger').addEventListener('click', () => {
+    document.querySelector('.nav__list').classList.toggle('show');
+    document.querySelector('.mobile-menu__btn').classList.toggle('open');
+});
